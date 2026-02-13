7633b57b244caed7cdc827b230ff07db785ce097
Kirill <52141481+CliqueOne@users.noreply.github.com>
2026-02-13 11:33:13 -0800

    Updated Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -181,7 +181,7 @@ export function Signup({ onToggle }: { onToggle: () => void }) {
         {referrerInfo && (
           <div className="bg-green-50 border border-green-200 rounded-lg p-4 mb-6">
             <p className="text-sm text-green-800">
-              <span className="font-semibold">{referrerInfo.name}</span> invited you to join{' '}
+              You've been invited to join{' '}
               <span className="font-semibold">{referrerInfo.companyName}</span> as a referrer
             </p>
           </div>
